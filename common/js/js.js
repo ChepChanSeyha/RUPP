d9c44b518fe1bf8568f813ab34c34173f8ae0071
CalvinSey <chepchanseyha@example.com>
2019-01-28 10:29:28 +0700

    1/28/1029

--- common/js/js.js
+++ common/js/js.js
@@ -1,5 +1,5 @@
-// Read More
-function myFunction() {
+// Read More overview
+function myFunction1() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
@@ -13,4 +13,38 @@ function myFunction() {
       btnText.innerHTML = "Read less"; 
       moreText.style.display = "inline";
     }
+}
+
+// Read More overview
+function myFunction2() {
+  var dots1 = document.getElementById("dots1");
+  var moreText1 = document.getElementById("more1");
+  var btnText1 = document.getElementById("myBtn1");
+
+  if (dots1.style.display === "none") {
+    dots1.style.display = "inline";
+    btnText1.innerHTML = "Read more"; 
+    moreText1.style.display = "none";
+  } else {
+    dots1.style.display = "none";
+    btnText1.innerHTML = "Read less"; 
+    moreText1.style.display = "inline";
   }
+}
+
+// Read More overview
+function myFunction3() {
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("myBtn2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = "Read more"; 
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "Read less"; 
+    moreText2.style.display = "inline";
+  }
+}
